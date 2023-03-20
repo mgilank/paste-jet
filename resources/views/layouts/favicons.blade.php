f2f7a5d6b2f24c67dc75b38adc86768b400e5e86
mahardhika gilang <gilang@jetorbit.com>
2023-03-20 12:33:19 +0700

    update

--- resources/views/layouts/favicons.blade.php
+++ resources/views/layouts/favicons.blade.php
@@ -2,6 +2,6 @@
 <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="mask-icon" href="https://www.jetorbit.com/wp-content/uploads/2021/01/favicon-150x150.png" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#2b5797">
 <meta name="theme-color" content="#ffffff">
