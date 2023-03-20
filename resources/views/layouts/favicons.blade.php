937f77034dc7c5ddfd3af51d7b1920e87f38bd27
mahardhika gilang <gilang@jetorbit.com>
2023-03-20 12:34:41 +0700

    update

--- resources/views/layouts/favicons.blade.php
+++ resources/views/layouts/favicons.blade.php
@@ -1,6 +1,6 @@
-<link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://www.jetorbit.com/wp-content/uploads/2021/01/favicon-150x150.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://www.jetorbit.com/wp-content/uploads/2021/01/favicon-150x150.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://www.jetorbit.com/wp-content/uploads/2021/01/favicon-150x150.png">
 <link rel="manifest" href="/site.webmanifest">
 <link rel="mask-icon" href="https://www.jetorbit.com/wp-content/uploads/2021/01/favicon-150x150.png" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#2b5797">
